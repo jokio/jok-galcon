1822d14c59bb6fe1437201eb0d5c48149ddc7ce2
Ezeki Zibzibadze <ez@jok.io>
2013-11-21 03:04:28 +0400

    added jok authorization

--- Jok.Galcon/Controllers/HomeController.cs
+++ Jok.Galcon/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Jok.GameEngine;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,9 +16,43 @@ namespace Jok.Galcon.Controllers
             return RedirectToAction("Play");
         }
 
-        public ActionResult Play(string channel, int userid = 32)
+        public ActionResult Play(string id, string sid)
         {
-            ViewBag.UserID = userid;
+            var loginUrl = ConfigurationManager.AppSettings["jok:SiteUrl"] + "/joinus?returnUrl=" + Request.Url;
+            var exitUrl = ConfigurationManager.AppSettings["jok:SiteUrl"] + "/lobby/japmatrix";
+
+            if (!String.IsNullOrEmpty(sid))
+            {
+                var cookie = Request.Cookies["sid"];
+                if (cookie == null)
+                    cookie = new HttpCookie("sid", sid);
+
+                cookie.Value = sid;
+                cookie.Expires = DateTime.UtcNow.AddYears(30);
+
+                if (Request.Url.Host.Contains('.'))
+                    cookie.Domain = Request.Url.Host.Substring(Request.Url.Host.IndexOf('.'));
+
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                sid = Request.Cookies["sid"] == null ? "" : Request.Cookies["sid"].Value;
+            }
+
+            var userInfo = JokAPI.GetUser(sid, Request.UserHostAddress);
+            if (userInfo == null || !userInfo.IsSuccess)
+                return Redirect(loginUrl);
+
+            //if (!String.IsNullOrEmpty(userInfo.CultureName))
+            //{
+            //    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(userInfo.CultureName);
+            //    ViewBag.Language = userInfo.CultureName.Replace('-', '_');
+            //}
+
+            ViewBag.Channel = id;
+            ViewBag.AuthorizationUrl = loginUrl;
+            ViewBag.ExitUrl = exitUrl;
 
             return View();
         }
--- Jok.Galcon/GameServer/GameHub.cs
+++ Jok.Galcon/GameServer/GameHub.cs
@@ -9,23 +9,6 @@ namespace Jok.Galcon.GameServer
 {
     public class GameHub : GameHubBase<GameTable>
     {
-        #region Custom Authentication
-        // ტესტირებისთვის არის ეს მხოლოდ საჭირო, რეალურ სერვერზე რომ არ შეამოწმოს ინფო
-        protected override JokUserInfo GetUserInfo(string token, string ipaddress)
-        {
-            var userInfo = new JokUserInfo
-            {
-                IsSuccess = true,
-                UserID = Convert.ToInt32(token),
-                Nick = token,
-                IsVIP = false
-            };
-
-            return userInfo;
-        }
-        #endregion
-
-
         public void Move(Guid from, Guid to, int count)
         {
             var user = GetCurrentUser();
