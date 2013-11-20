5413fd3f5ffc173f4945276da090d166f8e07f88
Levan Ilashvili <ilashvili.levan@gmail.com>
2013-11-21 02:35:57 +0400

    Commit Model

---
+++ Jok.Galcon/GameServer/Models/Planet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jok.Galcon.GameServer.Models
+{
+    public class Planet
+    {
+        public Guid ID { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Radius { get; set; }
+        public int ShipCount { get; set; }
+        public int GroupID { get; set; }
+    }
+}
