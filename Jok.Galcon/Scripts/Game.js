92f337e7f2dd5f4c9309af185c0fc7c93a864474
Ezeki Zibzibadze <ez@jok.io>
2013-11-21 01:15:45 +0400

    added nuget package restore

--- Jok.Galcon/Scripts/Game.js
+++ Jok.Galcon/Scripts/Game.js
@@ -3,10 +3,21 @@ var Game = {
 
     Init: function () {
         var proxy = new GameHub('GameHub', window.userid, '');
+        proxy.on('Online', this.Online.bind(this));
+        proxy.on('Offline', this.Offline.bind(this));
 
         proxy.start();
-    }
+    },
 
+
+    // Server Callbacks ----------------------------------------
+    Online: function () {
+        console.log('server is online');
+    },
+
+    Offline: function () {
+        console.log('server is offline');
+    }
 }
 
 Game.Init();
